794055d9629d2552a4e9474331760b299034e138
Marcello Messori <marcello.mm.messori@gmail.com>
2014-05-30 17:07:51 +0200

    team-assessment_maven: get header and lines as strings

--- team-assessment_maven/src/main/java/com/marcellomessori/teamassessmentassignment/MyCsv.java
+++ team-assessment_maven/src/main/java/com/marcellomessori/teamassessmentassignment/MyCsv.java
@@ -1,20 +1,42 @@
 package com.marcellomessori.teamassessmentassignment;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 
 class MyCsv {
 	private final URL csvPersonsFile;
+	private String csvHeaderLine;
+	private ArrayList<String> csvDataLines;
 
-	public MyCsv(String csvPersonsFileName) throws FileNotFoundException {
+	public MyCsv(String csvPersonsFileName) throws FileNotFoundException, IOException {
 		csvPersonsFile = getClass().getClassLoader().getResource(csvPersonsFileName);
 		if (csvPersonsFile == null) {
 			throw new FileNotFoundException("Error: could not find '" + csvPersonsFileName + "'.");
 		}
+		init(csvPersonsFile);
 	}
 	
 	public String view() {
 		return "Found " + csvPersonsFile;
 	}
 
+	String getHeaderString() {
+		return csvHeaderLine;
+	}
+
+	private void init(URL csvPersonsFile) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(csvPersonsFile.getPath()));
+		
+		csvHeaderLine = br.readLine();
+		
+		csvDataLines = new ArrayList<String>();
+		String line;
+		while ((line = br.readLine()) != null) {
+			csvDataLines.add(line);
+		}
+	}
 }
--- team-assessment_maven/src/test/java/com/marcellomessori/teamassessmentassignment/MyCsvTest.java
+++ team-assessment_maven/src/test/java/com/marcellomessori/teamassessmentassignment/MyCsvTest.java
@@ -2,29 +2,40 @@ package com.marcellomessori.teamassessmentassignment;
 
 import static org.junit.Assert.*;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class MyCsvTest {
 
-    @Before
-    public void setUp() {
-    }
+	private MyCsv myCsv;
+
+	@Before
+	public void setUp() throws FileNotFoundException, IOException {
+		myCsv = new MyCsv("persons.csv");
+	}
 
 	@Test
-	public void shoudReturnFileNotFoundExceptionWhenItCannotFindTheSourceFile() {
+	public void shoudReturnFileNotFoundExceptionWhenItCannotFindTheSourceFile() throws IOException {
 		try {
 			new MyCsv("nonExistingFile.csv");
 			fail("Should have thrown a FileNotFoundException.");
-		}
-		catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			assertEquals(true, true);
 		}
 	}
 
+	@Test
+	public void shoudReturnAnHeaderFileCOntainingTheBasicFields() {
+		String header = myCsv.getHeaderString();
+		assertTrue(header.contains("Name"));
+		assertTrue(header.contains("Age"));
+		assertTrue(header.contains("City"));
+	}
+
 	@Ignore
 	public void nextTest() {
-		
+
 	}
 }
